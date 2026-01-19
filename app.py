d0dc19ef2c4a516c192a7dcabbc2e483afcf4bc1
CuongGustav <cuonggustav@gmail.com>
2026-01-19 22:09:19 +0700

    change app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+import os
 from src import create_app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=5000, debug=True) 
+    port = int(os.environ.get("PORT", 5000))
+    
+    is_production = "production"
+    debug_mode = not is_production
+    
+    app.run(host="0.0.0.0", port=port, debug=debug_mode)
